828faa4d00040b6a8d40ccf8193fa3bf75807113
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-05 23:39:47 +0530

    Update HappyNumbers.py

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,1 +1,19 @@
+class Solution:
+    def ishappy(self, n):
+        seen = set()
 
+        while n != 1 and n not in seen:
+            seen add(n)
+            n = self.square_sum_of_digits(n)
+
+        return n == 1
+
+    def square_sum_of_digits(self, num):
+        total = 0
+        while num > 0:
+            digit = num % 10
+            total += digit * digit
+            num //= 10
+        return total
+
+solution = Solution()
