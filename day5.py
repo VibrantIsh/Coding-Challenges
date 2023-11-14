1dc2af7309034503c30c0a8ba3c982a8550abd05
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-14 13:23:19 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,11 +1,9 @@
 # https://www.codechef.com/practice/course/arrays-strings-sorting/INTARR01/problems/EQUALELE
 from collections import Counter
-
 t = int(input())
 for _ in range(t):
     n = int(input())
     a = input().split()
-    
     c = Counter(a)
     m = max(c.values())
     print(n - m)
