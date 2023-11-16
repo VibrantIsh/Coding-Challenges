4213415a102d250e5cc8df8e6e616947b46f62a6
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-16 16:52:28 +0530

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,1 +1,5 @@
+# https://www.codechef.com/practice/course/2to3stars/LP2TO301/problems/ALTER
 
+for _ in range(int(input())):
+    a, b, p, q = map(int, input().split())
+    print("YES" if (p % a == 0 and q % b == 0 and (p // a == q // b or p // a == q // b + 1 or p // a + 1 == q // b)) else "NO")
