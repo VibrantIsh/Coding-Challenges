14fe102d66642c9b2b8a9efd6754932525a8d9b4
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-17 11:21:15 +0530

    Update day7.py

--- day7.py
+++ day7.py
@@ -1,1 +1,4 @@
-
+# https://www.codechef.com/practice/course/strings-cpp/PCPPST01/problems/TWOSTR
+for _ in range(int(input())):
+    x, y = input(), input()
+    print("Yes" if all(a == b or '?' in {a, b} for a, b in zip(x, y)) else "No")
