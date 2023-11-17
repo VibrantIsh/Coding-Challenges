9601b0421e8865c814b86084971a0098937dbcdc
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-17 13:08:29 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -1,1 +1,11 @@
+# https://www.codechef.com/problems/ZEROSTRING
+for _ in range(int(input())):
+    a=int(input())
+    b=input()
+    c=b.count("0")
+    d=b.count("1")
+    if c>=d:
+        print(a-c)
+    else:
+        print(a-d+1)
 
