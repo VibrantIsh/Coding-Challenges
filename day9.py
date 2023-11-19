009cdc855374905dfa02d24b64e51be26d7a93ba
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-19 23:18:38 +0530

    Update day9.py

--- day9.py
+++ day9.py
@@ -1,1 +1,12 @@
+# https://www.codechef.com/problems/COUPON2
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    c = sum(map(int, input().split()))
+    d = sum(map(int, input().split()))
+    if c >= 150 and d >= 150:
+        print("YES" if c + d + b < c + d + 2 * a else "NO")
+    elif c < 150 and d < 150:
+        print("NO")
+    else:
+        print("YES" if c + d + b + a < c + d + 2 * a else "NO")
 
