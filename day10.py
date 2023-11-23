8e5f0e8654acbf0268fdd1c584584243a2338b07
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-23 12:02:52 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,1 +1,19 @@
+class P:
+    def c(self, n, a):
+        return "YES" if all(a.count(x) % 2 == 0 for x in set(a)) else "NO"
+
+
+def m():
+    t = int(input())
+    p = P()
+    
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        r = p.c(n, a)
+        print(r)
+
+
+if __name__ == "__main__":
+    m()
 
