cf2aadba28e11f095a6c66942b2c8573f35dd846
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-24 23:34:39 +0530

    Update day11.py

--- day11.py
+++ day11.py
@@ -1,1 +1,7 @@
+# https://www.codechef.com/practice/course/two-pointers/POINTERF/problems/PREP17
+for _ in range(int(input())):
+    N = int(input())
+    A = set(map(int, input().split()))
+    B = set(map(int, input().split()))
+    print(len(A.intersection(B)))
 
