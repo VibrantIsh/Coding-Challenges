1a4f6a91399d1041f87bb18efe197e2590ccadf9
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-26 18:47:56 +0530

    Update day13.py

--- day13.py
+++ day13.py
@@ -1,3 +1,4 @@
+#https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/SMOL?tab=statement
 for _ in range(int(input())):
     n, k = map(int, input().split())
     print(n if k == 0 else n % k if n >= k else n)
