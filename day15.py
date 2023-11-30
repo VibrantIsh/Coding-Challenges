8cc44b6e9c698dd1b06bd9b72b671d77da359972
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-30 21:18:52 +0530

    Update day15.py

--- day15.py
+++ day15.py
@@ -1,1 +1,6 @@
-day15.py
+# https://www.codechef.com/practice/course/greedy-algorithms/INTGRA01/problems/MAXDIFF
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    l = sorted(map(int, input().split()))
+    c = min(k, n - k)
+    print(sum(l[c:]) - sum(l[:c]))
