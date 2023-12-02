9570dddd3c6f00217eb40fe89e0e68f6f6fc7fcf
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-02 23:55:18 +0530

    Update day17.py

--- day17.py
+++ day17.py
@@ -1,1 +1,4 @@
-
+# https://www.codechef.com/practice/course/interview-dsa/DSAPREP_01/problems/MONSTER1
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    print(0 if c >= b else 1)
