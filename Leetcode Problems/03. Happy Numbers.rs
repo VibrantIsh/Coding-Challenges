ec2ffd1cd35a95de8859fff2194934be318e73fb
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-11 23:09:18 +0530

    Update 03. Happy Numbers.rs

--- Leetcode Problems/03. Happy Numbers.rs
+++ Leetcode Problems/03. Happy Numbers.rs
@@ -1,1 +1,24 @@
+// https://leetcode.com/problems/happy-number/
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        fn get_next(mut n: i32) -> i32 {
+            let mut sum = 0;
+            while n > 0 {
+                let digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            sum
+        }
 
+        let mut slow = n;
+        let mut fast = get_next(n);
+
+        while fast != 1 && slow != fast {
+            slow = get_next(slow);
+            fast = get_next(get_next(fast));
+        }
+
+        fast == 1
+    }
+}
