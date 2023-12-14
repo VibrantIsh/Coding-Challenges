6dda7ea4d4e4d3b2f1191fa73955484d8e6e1e8c
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-14 21:50:00 +0530

    Update 06. Fibonacci-number.py

--- Leetcode Problems/06. Fibonacci-number.py
+++ Leetcode Problems/06. Fibonacci-number.py
@@ -1,1 +1,12 @@
 # https://leetcode.com/problems/fibonacci-number/
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        
+        a, b = 0, 1
+        for _ in range(n - 1):
+            a, b = b, a + b
+        
+        return b
+
