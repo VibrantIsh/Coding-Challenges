161888d56b28914b679952c71a181f1b1acab277
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-15 21:54:48 +0530

    Update 07. Robot Return to Origin.py

--- Leetcode Problems/07. Robot Return to Origin.py
+++ Leetcode Problems/07. Robot Return to Origin.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/robot-return-to-origin/
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        i = j = 0
+        for a in moves:
+            if a == 'L':
+                j -= 1
+            elif a == 'R':
+                j += 1
+            elif a == 'U':
+                i -= 1
+            else:
+                i += 1
+        return i == j == 0
