1d1f9a046d74020003961642ec595bb3f7eb495a
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-17 23:55:04 +0530

    Update 09. Maximum 69 Number .py

--- Leetcode Problems/09. Maximum 69 Number .py
+++ Leetcode Problems/09. Maximum 69 Number .py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/maximum-69-number/
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        digits_list = list(str(num))
+
+        for i in range(len(digits_list)):
+            if digits_list[i] == '6':
+                digits_list[i] = '9'
+                break
+
+        return int(''.join(digits_list))
