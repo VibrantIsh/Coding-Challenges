0e2e7a6e68494d0d49704f21201edd9e2a1214ab
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-19 22:53:19 +0530

    Update 11. Ugly Number.rs

--- Leetcode Problems/11. Ugly Number.rs
+++ Leetcode Problems/11. Ugly Number.rs
@@ -1,1 +1,14 @@
-
+# https://leetcode.com/problems/ugly-number/
+impl Solution {
+    pub fn is_ugly(mut n: i32) -> bool {
+        if n < 1 {
+            return false;
+        }
+        for prime in [2, 3, 5].iter() {
+            while n % prime == 0 {
+                n /= prime;
+            }
+        }
+        n == 1
+    }
+}
