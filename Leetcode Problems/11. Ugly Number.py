38285cc841ea9931e5e9d5e3214791593e023e9f
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-19 22:39:37 +0530

    Update 11. Ugly Number.py

--- Leetcode Problems/11. Ugly Number.py
+++ Leetcode Problems/11. Ugly Number.py
@@ -1,1 +1,18 @@
+# https://leetcode.com/problems/ugly-number/
+class Solution:
+    def isUgly(self,n:int)->bool:
+        if n == 1:
+            return True 
+        if n <= 0:
+            return False
+        
+        while n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+            if n % 2 == 0:
+                n//=2
+            if n % 3 == 0:
+                n //= 3
+            if n % 5 == 0:
+                n//= 5
+        
+        return n == 1
 
