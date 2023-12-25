f6141b3ee82b85ca27f50a8a32d7abdbe8ca92c7
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-25 23:49:08 +0530

    Update 23. Select data.py

--- Leetcode Problems/23. Select data.py
+++ Leetcode Problems/23. Select data.py
@@ -1,1 +1,4 @@
-
+# https://leetcode.com/problems/select-data/
+import pandas as pd
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students["student_id"]==101,["name","age"]]
