68ae9ce05ae1ef37a29d4815828894627d87fb36
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-25 23:59:23 +0530

    Create 24. permutations-ii.py

---
+++ Leetcode Problems/24. permutations-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        c = []
+        
+        def y(nums, temp):
+            if len(nums) == 0:
+                c.append(temp)
+                return
+            for i in range(len(nums)):
+                y(nums[:i] + nums[i+1:], temp + [nums[i]])
+                
+        y(nums, [])
+        return set(tuple(ele) for ele in c)
