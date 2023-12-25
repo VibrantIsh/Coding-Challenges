0a6726f0b6e348c41f7fb0dad6eb7935c72ee774
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-25 14:34:36 +0530

    Update 17.  Merge Sorted Array.py

--- Leetcode Problems/17.  Merge Sorted Array.py
+++ Leetcode Problems/17.  Merge Sorted Array.py
@@ -1,1 +1,8 @@
-
+# https://leetcode.com/problems/merge-sorted-array/
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        x = len(nums1) - m
+        for i in range(x):
+            nums1.pop()
+        nums1.extend(nums2)
+        nums1.sort()
