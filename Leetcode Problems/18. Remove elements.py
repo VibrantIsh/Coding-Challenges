165b453f1b116ec300bc788fcb722bf6c9309e31
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-25 23:42:13 +0530

    Update 18. Remove elements.py

--- Leetcode Problems/18. Remove elements.py
+++ Leetcode Problems/18. Remove elements.py
@@ -1,1 +1,7 @@
+# https://leetcode.com/problems/remove-element/
+class Solution:
+    def removeElement(self, nums: List[int], x: int) -> int:
+        # Keep ditching 'x' from 'nums' until it's all gone
+        while x in nums:
+            nums.remove(x)
 
