5d49a2225170c94a677549766732a7150347ce46
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-26 23:40:37 +0530

    Create 30. Greatest Common Divisor of Strings.py

---
+++ Leetcode Problems/30. Greatest Common Divisor of Strings.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def gcd(a, b):
+            if b == 0:
+                return a
+            return gcd(b, a % b)
+
+        gcd_length = gcd(len(str1), len(str2))
+        gcd_str = str1[:gcd_length]
+
+        return gcd_str if str1 == gcd_str * (len(str1) // gcd_length) and str2 == gcd_str * (len(str2) // gcd_length) else ""
