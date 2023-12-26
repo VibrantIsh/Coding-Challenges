66d71eba3e875b5b2f7da420ed73bf9398be2013
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-26 23:35:18 +0530

    Update 27. Find Common Characters.py

--- Leetcode Problems/27. Find Common Characters.py
+++ Leetcode Problems/27. Find Common Characters.py
@@ -1,1 +1,4 @@
-
+# https://leetcode.com/problems/find-common-characters/
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+           return (list(reduce(lambda x, y: x & Counter(y), words, Counter(words[0])).elements())if words else [])
