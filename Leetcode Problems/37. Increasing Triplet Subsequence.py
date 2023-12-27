6e436faaf8159b3a7f8d327cbe64f3c332a24c82
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-27 23:24:52 +0530

    Update 37. Increasing Triplet Subsequence.py

--- Leetcode Problems/37. Increasing Triplet Subsequence.py
+++ Leetcode Problems/37. Increasing Triplet Subsequence.py
@@ -1,1 +1,13 @@
+# https://leetcode.com/problems/increasing-triplet-subsequence/
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = None
+        for n in nums:
+            if first is None or n <= first:
+                first = n
+            elif second is None or n <= second:
+                second = n
+            else:
+                return True
+        return False
 
