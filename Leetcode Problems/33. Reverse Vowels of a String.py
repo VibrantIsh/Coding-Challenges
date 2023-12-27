09c3623a38e009628d94da0d596bf95db220b2c9
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-27 23:12:42 +0530

    Update 33. Reverse Vowels of a String.py

--- Leetcode Problems/33. Reverse Vowels of a String.py
+++ Leetcode Problems/33. Reverse Vowels of a String.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/reverse-vowels-of-a-string/
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = "aeiouAEIOU"
+        reverse = [c for c in s if c in vowels][::-1]
+        string = ''
+        idx = 0
+        for x in s:
+            if x in vowels:
+                string += reverse[idx]
+                idx += 1
+            else:
+                string += x
+        return string
+
