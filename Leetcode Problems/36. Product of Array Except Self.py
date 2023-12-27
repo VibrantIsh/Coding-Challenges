5e357ee80ecd7916c48aeb2ce036916c88f31121
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-27 23:23:13 +0530

    Update 36. Product of Array Except Self.py

--- Leetcode Problems/36. Product of Array Except Self.py
+++ Leetcode Problems/36. Product of Array Except Self.py
@@ -1,1 +1,12 @@
 # https://leetcode.com/problems/product-of-array-except-self/
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        total_product = reduce(lambda x, y: x * y if y != 0 else x, nums, 1)
+        zero_count = nums.count(0)
+        
+        if zero_count > 1:
+            return [0] * len(nums)
+        elif zero_count == 1:
+            return [total_product if num == 0 else 0 for num in nums]
+        else:
+            return [total_product // num for num in nums]
