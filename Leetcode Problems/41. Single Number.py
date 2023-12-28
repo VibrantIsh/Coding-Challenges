4c2787b811392babdf9b90d2233ddbef0c214be9
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-29 00:43:41 +0530

    Update 41. Single Number.py

--- Leetcode Problems/41. Single Number.py
+++ Leetcode Problems/41. Single Number.py
@@ -1,1 +1,7 @@
-4
+# https://leetcode.com/problems/single-number/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x = 0
+        for num in nums:
+            x ^= num  # XOR operation cancels out duplicates
+        return x
