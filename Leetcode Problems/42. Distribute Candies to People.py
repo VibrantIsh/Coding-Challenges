218bce4edda3450e496645cb2aed53839279c4cb
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-30 23:43:15 +0530

    Update 42. Distribute Candies to People.py

--- Leetcode Problems/42. Distribute Candies to People.py
+++ Leetcode Problems/42. Distribute Candies to People.py
@@ -1,1 +1,11 @@
-#
+# https://leetcode.com/problems/distribute-candies-to-people/description/
+
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        a, i, c = [0] * num_people, 0, 1
+        while candies > 0:
+            a[i % num_people] += min(candies, c)
+            candies -= c
+            c += 1
+            i += 1
+        return a
