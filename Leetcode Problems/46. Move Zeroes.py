bd5f010b40995074d9b6463916b0a87f6fe1f473
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-31 20:14:56 +0530

    Update 46. Move Zeroes.py

--- Leetcode Problems/46. Move Zeroes.py
+++ Leetcode Problems/46. Move Zeroes.py
@@ -1,1 +1,7 @@
+# https://leetcode.com/problems/move-zeroes/
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        x = nums.count(0)
+        nums[:] = [num for num in nums if num != 0]
+        nums.extend([0] * x)
 
