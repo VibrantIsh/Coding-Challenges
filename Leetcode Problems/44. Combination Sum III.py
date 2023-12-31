3e15a5f293fcb1107eb457d0c7068a314e6c6fbb
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-12-31 20:12:27 +0530

    Update 44. Combination Sum III.py

--- Leetcode Problems/44. Combination Sum III.py
+++ Leetcode Problems/44. Combination Sum III.py
@@ -1,1 +1,11 @@
-
+# https://leetcode.com/problems/combination-sum-iii/ 
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        x = [i for i in range(1, 10)]
+        ans = []
+        
+        for i in itertools.combinations(x, k):
+            if sum(i) == n:
+                ans.append(list(i))
+        
+        return ans
