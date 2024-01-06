bf017ee34b7589d74013c96843b800065781f091
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2024-01-07 00:17:01 +0530

    Update 52. Drop Missing Data.py

--- Leetcode Problems/52. Drop Missing Data.py
+++ Leetcode Problems/52. Drop Missing Data.py
@@ -1,1 +1,5 @@
-52. 
+# https://leetcode.com/problems/drop-missing-data/
+import pandas as pd
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    x = students.dropna(subset = ['name'])
+    return x
