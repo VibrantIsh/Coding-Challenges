b44b0aba178877044a1e776db56d4781c23a1d72
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2024-01-08 23:32:56 +0530

    Update 52. Modify Columns.py

--- Leetcode Problems/52. Modify Columns.py
+++ Leetcode Problems/52. Modify Columns.py
@@ -1,1 +1,5 @@
 # https://leetcode.com/problems/modify-columns/
+import pandas as pd
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary'] *2
+    return employees
