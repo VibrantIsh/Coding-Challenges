7e6e47c8fe6f06ba25ebe7a34040045944cd3030
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2024-01-10 23:06:46 +0530

    Update 55. Fill Missing Data.py

--- Leetcode Problems/55. Fill Missing Data.py
+++ Leetcode Problems/55. Fill Missing Data.py
@@ -1,1 +1,4 @@
 # https://leetcode.com/problems/fill-missing-data/
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products["quantity"]= products["quantity"].fillna(0)
+    return products
