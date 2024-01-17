b6bbf916743f8b9c9fd5320b43f89bebdca1a407
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2024-01-17 23:12:18 +0530

    Update 59. Can place Flowers.py

--- Leetcode Problems/59. Can place Flowers.py
+++ Leetcode Problems/59. Can place Flowers.py
@@ -1,1 +1,10 @@
-# 
+# https://leetcode.com/problems/can-place-flowers/
+class Solution:
+    def canPlaceFlowers(self, flowerbed, n):
+        count = 0
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1:i+2] == [0, 0, 0]:
+                flowerbed[i] = 1
+                count += 1
+        return count >= n
